42f64eedb0cb91dec2ee25a38809f4a6ae4c1757
Shaivpatel2711 <hardikpatel.patel2910@gmail.com>
2026-07-21 19:29:46 -0600

    lesson 5 activity 1

---
+++ main.py
@@ -0,0 +1,8 @@
+Happy = -600
+if Happy > 0 :
+    print("this number is positive :")
+print("this is not in the if block")
+
+sad = 600
+if sad > 0 :
+    print("This number is greater than 0 and positive")
