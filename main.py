8615bda9315e12daded11be113726d8b1be157d2
Shaivpatel2711 <hardikpatel.patel2910@gmail.com>
2026-07-21 19:46:28 -0600

    activity 2

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ print("this is not in the if block")
 
 sad = 600
 if sad > 0 :
-    print("This number is greater than 0 and positive")
+    print("This number is greater than 0 and positive")#activity 2 lesson 5
+actual_cost = float(input("enter the value of the item"))
+sale_amount = float(input("enter the sale amount"))
+if sale_amount > actual_cost :
+    profit= sale_amount - actual_cost
+    print("the profit is:", profit)
+else :
+    print("no profit only loss")
